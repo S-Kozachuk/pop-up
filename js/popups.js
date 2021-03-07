f2e373c0807c4444464852f3cf9a533d198cd26b
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-03-07 15:47:53 +0300

    POPUP SCRIPT / Declared the popup script

---
+++ js/popups.js
@@ -0,0 +1,11 @@
+const popupLinks = document.querySelectorAll('popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll("lock-padding");
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.lenght > 0) {
+    
+}

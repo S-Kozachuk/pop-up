0bd01e795b0d65ece251c43493cb825f98bcaa0e
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-03-08 17:20:05 +0300

    Added checked for .lockPadding object

--- js/popups.js
+++ js/popups.js
@@ -53,9 +53,11 @@ function bodyLock() {
     // Calculating the scrollbar width
     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
-    for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = lockPaddingValue;
+    if (lockPadding.lenght > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
     }
     body.style.paddingRight = lockPaddingValue;
     body.classList.add('lock');

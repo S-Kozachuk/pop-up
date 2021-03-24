4bf76d14939151bf350266e0cfe845ed0fc2cd63
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-03-24 17:52:52 +0300

    Rename wrong property in JS

--- js/popups.js
+++ js/popups.js
@@ -7,7 +7,7 @@ let unlock = true;
 const timeout = 800;
 
 if (popupLinks.lenght > 0) {
-    for (let index = 0; index < popupLinks.lenght; index++) {
+    for (let index = 0; index < popupLinks.length; index++) {
         const popupLink = popupLinks[index];
         popupLink.addEventListener("click", function (e) {
             const popupName = popupLink.getAttribute('href').replace('#', '');

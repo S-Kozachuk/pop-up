a06d7f3a697d7c08b29e13549ffafa05db584eb5
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-04-06 20:00:30 +0300

    Removed double quotes to .lock-padding class

--- popup/js/popups.js
+++ popup/js/popups.js
@@ -1,6 +1,6 @@
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+const lockPadding = document.querySelectorAll('.lock-padding');
 
 let unlock = true;
 
